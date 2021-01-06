e1e6dc6a3d68a518cd1dced0bb3045b1face63df
arnaupool <arnyr@hotmail.es>
2021-01-06 18:00:18 +0100

    A

--- WhacAMole/app/src/main/java/com/example/whac_a_mole/MyView.java
+++ WhacAMole/app/src/main/java/com/example/whac_a_mole/MyView.java
@@ -1,6 +1,7 @@
 package com.example.whac_a_mole;
 
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.View;
 import android.view.View;
 
@@ -17,6 +18,8 @@ public class MyView extends View{
     Random random = new Random();
     Paint paint = new Paint();
 
+    private GestureDetector gDet;
+
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         paint.setAntiAlias(true);
@@ -26,4 +29,43 @@ public class MyView extends View{
         paint.setStrokeJoin(Paint.Join.ROUND);
 
     }
+
+    @Override
+    protected void onDraw (Canvas cnv) {
+        super.onDraw(cnv);
+    }
+
+    @Override
+    public boolean onTouchEvent (MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+        }
+        return true;
+    }
+
+    private void setupGesture() {
+        gDet = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                //Notified when a double-tap occurs
+                return true;
+            }
+
+            @Override
+            public boolean onDoubleTapEvent(MotionEvent e) {
+                //Notified when an event within a double-tap gesture occurs
+                return true;
+            }
+        });
+
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return gDet.onTouchEvent(motionEvent);
+            }
+        });
+    }
+
+    private void validate() { this.invalidate(); }
 }
